b9274804ec28808a85f0826e82ee5d0d7c2ded6b
gppsb <963178771@qq.com>
2019-04-01 10:14:04 +0800

    4.1 10:13 the secend day

--- app/src/main/java/com/example/a325/fragments/CourseFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.a325.fragments;
-
-public class CourseFragment {
-}
--- app/src/main/java/com/example/a325/fragments/DownloadFragment.java
+++ app/src/main/java/com/example/a325/fragments/DownloadFragment.java
@@ -1,4 +1,41 @@
 package com.example.a325.fragments;
 
-public class DownloadFragment {
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a325.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DownloadFragment extends Fragment {
+
+
+    public DownloadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_download, container, false);
+        ButterKnife.bind(this,view);
+
+        getDiveceSize();
+
+        return view;
+    }
+
+    private void getDiveceSize(){
+
+    }
+
 }
